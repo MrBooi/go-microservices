4d3165a07fbf18ebe7eff74749b5c969a9d7d477
MrBooi <ayabongabooi2@gmail.com>
2025-12-27 23:23:27 +0200

    feat: connecting to rabbitmq

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -9,6 +9,8 @@ import (
 	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"ride-sharing/shared/env"
+	"ride-sharing/shared/messaging"
 	"syscall"
 
 	grpcserver "google.golang.org/grpc"
@@ -17,6 +19,8 @@ import (
 var GrpcAddr = ":9093"
 
 func main() {
+	rabbitMqURI := env.GetString("RABBITMQ_URI", "amqp://guest:guest@rabbitmq:5672/")
+
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
 
@@ -26,7 +30,7 @@ func main() {
 	go func() {
 		sigCh := make(chan os.Signal, 1)
 		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
-		<-sigCh // block the program until it receive an message
+		<-sigCh
 		cancel()
 	}()
 
@@ -35,7 +39,16 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	// Starting Grpc server
+	// RabbitMQ connection
+	rabbitmq, err := messaging.NewRabbitMQ(rabbitMqURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitmq.Close()
+
+	log.Println("Starting RabbitMQ connection")
+
+	// Starting the gRPC server
 	grpcServer := grpcserver.NewServer()
 	grpc.NewGRPCHandler(grpcServer, svc)
 
