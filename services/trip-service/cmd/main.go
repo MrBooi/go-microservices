b811c77199b567388b1752b41760ba2e73427bf5
MrBooi <ayabongabooi2@gmail.com>
2025-12-30 07:33:40 +0200

    feat: mongodb repository

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -51,8 +51,11 @@ func main() {
 
 	rabbitMqURI := env.GetString("RABBITMQ_URI", "amqp://guest:guest@rabbitmq:5672/")
 
-	inmemRepo := repository.NewInmemRepository()
-	svc := service.NewService(inmemRepo)
+	//inmemRepo := repository.NewInmemRepository()
+	//svc := service.NewService(inmemRepo)
+
+	mongoDBRepo := repository.NewMongoRepository(mongoDb)
+	svc := service.NewService(mongoDBRepo)
 
 	go func() {
 		sigCh := make(chan os.Signal, 1)
